6282186e85e818787e891cb8929b81239efc30a1
Lyon Etyo <mulyonoeko.prastyo@gmail.com>
2022-04-13 05:46:18 +0700

    Fix, Error message show up meanwhile service worker is succesfully registered

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", async _ => {
     try {
       await navigator.serviceWorker.register("/sw.js");
-      console.log("Service Worker Berhasil Didaftarkan!", serviceWorker);
+      console.log("Service Worker Berhasil Didaftarkan!");
     } catch (error) {
       console.error("Service Worker Gagal Didaftarkan!", error);
     }
